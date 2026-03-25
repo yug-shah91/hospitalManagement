43216cf1751e2258b8d24ccffd53728b86b38d56
Yug Shah <shahyug91@gmail.com>
2026-03-25 09:52:39 +0530

    User Repo

---
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.hospitalManagement.repository;
+
+import com.example.hospitalManagement.entity.User;
+import com.example.hospitalManagement.entity.type.AuthProviderType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByProviderIdAndProviderType(String providerId, AuthProviderType providerType);
+}
