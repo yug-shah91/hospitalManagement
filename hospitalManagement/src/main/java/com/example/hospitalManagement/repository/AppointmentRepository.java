83d12e07f2c435d182aa66deb5d1e261ad6fcac6
Yug Shah <shahyug91@gmail.com>
2026-03-25 09:51:19 +0530

    Change in repo

--- hospitalManagement/src/main/java/com/example/hospitalManagement/repository/AppointmentRepository.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/repository/AppointmentRepository.java
@@ -3,5 +3,8 @@ package com.example.hospitalManagement.repository;
 import com.example.hospitalManagement.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorId(Long doctorId);
 }
